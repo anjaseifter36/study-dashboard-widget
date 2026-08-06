4fe0ccdc8bdf44a93dda22a4342ddbcb8427cfdc
anjaseifter36 <anjaseifter36@gmail.com>
2026-08-06 10:59:42 +0000

    Allow Notion embed

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,7 +1,23 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self' https://www.notion.so https://notion.so;",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "ALLOW-FROM https://www.notion.so",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
